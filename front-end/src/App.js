921c745fe14513e56b9a622f59fe942c50ee3508
hahava <hahava5696@gmail.com>
2021-03-15 17:54:59 +0900

    Make component for login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
+      <Login></Login>
     </div>
   );
 }
