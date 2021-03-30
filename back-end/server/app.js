23da153dfe99391f970ae898b6070a69c36787bd
hahava <hahava5696@gmail.com>
2021-03-30 18:35:08 +0900

    Set mongodb

--- back-end/server/app.js
+++ back-end/server/app.js
@@ -3,6 +3,11 @@ import 'core-js/stable';
 import express from 'express';
 import path from 'path';
 import 'regenerator-runtime/runtime';
+import dotEnv from 'dotenv';
+import mongoose from 'mongoose';
+import loginRouter from './api/auth/route/loginRoute';
+
+dotEnv.config({ path: path.join(__dirname, './.env') });
 
 const app = express();
 
@@ -10,8 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'front')));
-
-// setup view engine
 app.set('views', path.join(__dirname, '/front'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -19,4 +22,18 @@ app.engine('html', require('ejs').renderFile);
 app.get('*', (req, res) => {
   res.render('index.html');
 });
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch(e => {
+    console.error(e);
+  });
+
 export default app;
