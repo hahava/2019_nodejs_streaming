b87bf491d2c9b156589b041728ec10f4c520a5ef
hahava <hahava5696@gmail.com>
2026-03-28 22:45:56 +0900

    refactor: remove nodemailer integration from backend

--- back-end/server/routes/board.js
+++ back-end/server/routes/board.js
@@ -1,16 +1,10 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
-import mailer from '../common/util/nodeMailer.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    await mailer.sendMail();
-    res.status(StatusCodes.OK).send('success!');
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('failed');
-  }
+router.get('/', (_req, res) => {
+  res.status(StatusCodes.OK).send('success!');
 });
 
 export default router;
