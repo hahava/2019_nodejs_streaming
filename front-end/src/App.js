5934a95de59a12caff96e94c29d9034f93cae46f
hahava <hahava5696@gmail.com>
2026-03-28 22:41:58 +0900

    refactor: optimize frontend webpack and route lazy loading

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import Login from './components/Login';
-import Error from './components/Error';
-import Videos from './components/Videos';
-import Home from './components/Home';
-import Player from './components/Player';
 import commonUtil from './common/commonUtil';
+
+const Login = lazy(() => import('./components/Login'));
+const Error = lazy(() => import('./components/Error'));
+const Videos = lazy(() => import('./components/Videos'));
+const Home = lazy(() => import('./components/Home'));
+const Player = lazy(() => import('./components/Player'));
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -22,13 +23,15 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <PrivateRoute path={['/index', '/']} exact component={Home} />
-          <PrivateRoute path="/video/:type" exact component={Videos} />
-          <PrivateRoute path="/video/:type/player/:fileName" exact component={Player} />
-          <Route path="/login" exact component={Login} />
-          <Route component={Error} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <PrivateRoute path={['/index', '/']} exact component={Home} />
+            <PrivateRoute path="/video/:type" exact component={Videos} />
+            <PrivateRoute path="/video/:type/player/:fileName" exact component={Player} />
+            <Route path="/login" exact component={Login} />
+            <Route component={Error} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -4,17 +4,27 @@ const path = require('path');
 module.exports = (_env, argv = {}) => {
   const mode = argv.mode || 'development';
   const isProduction = mode === 'production';
+  const outputPath = path.resolve(__dirname, '../back-end/server/front');
+  const publicPath = '/';
 
   return {
     mode,
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-      path: path.resolve(__dirname, '../back-end/server/front'),
-      filename: 'main.js',
+      path: outputPath,
+      filename: isProduction ? 'js/[name].[contenthash:8].js' : 'js/[name].js',
+      chunkFilename: isProduction ? 'js/[name].[contenthash:8].chunk.js' : 'js/[name].chunk.js',
       clean: true,
-      publicPath: '/',
+      publicPath,
     },
     devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    cache: {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename],
+      },
+    },
+    stats: 'errors-warnings',
     resolve: {
       extensions: ['.js', '.json'],
     },
@@ -26,6 +36,8 @@ module.exports = (_env, argv = {}) => {
           use: {
             loader: 'babel-loader',
             options: {
+              cacheDirectory: true,
+              cacheCompression: false,
               presets: ['@babel/preset-env', '@babel/preset-react'],
             },
           },
@@ -34,19 +46,40 @@ module.exports = (_env, argv = {}) => {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
+        {
+          test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[name].[contenthash:8][ext]',
+          },
+        },
       ],
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'public/index.html'),
+        minify: isProduction
+          ? {
+            collapseWhitespace: true,
+            removeComments: true,
+            useShortDoctype: true,
+          }
+          : false,
       }),
     ],
-    devServer: {
-      static: {
-        directory: path.resolve(__dirname, 'public'),
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
       },
+      runtimeChunk: 'single',
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
       historyApiFallback: true,
       hot: true,
+      compress: true,
+      overlay: true,
+      publicPath,
       port: 3000,
     },
   };
