811efc1c64af6ff6cb83a2bf7bd659c2ad45612c
hahava <hahava5696@gmail.com>
2026-03-28 22:16:58 +0900

    refactor(routes): harden legacy route handlers

--- back-end/server/routes/board.js
+++ back-end/server/routes/board.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import { StatusCodes } from 'http-status-codes';
 import mailer from '../common/util/nodeMailer';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  mailer.sendMail()
-    .catch(console.error);
-  res.send('success!');
+router.get('/', async (req, res) => {
+  try {
+    await mailer.sendMail();
+    res.status(StatusCodes.OK).send('success!');
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('failed');
+  }
 });
 
 export default router;
