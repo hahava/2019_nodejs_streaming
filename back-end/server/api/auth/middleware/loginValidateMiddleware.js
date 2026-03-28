f3e62e181fc0daa9a2a1529a0b90597dc97133b8
hahava <hahava5696@gmail.com>
2026-03-28 22:06:37 +0900

    refactor: simplify login validation middleware

--- back-end/server/api/auth/middleware/loginValidateMiddleware.js
+++ back-end/server/api/auth/middleware/loginValidateMiddleware.js
@@ -2,20 +2,19 @@ import { StatusCodes } from 'http-status-codes';
 import commonUtil from '../../../common/util/commonUtil';
 import ErrorMessage from '../../../common/util/errorMessage';
 
-const loginValidateMiddleware = (req, res, next) => {
-  const { userId, password } = req.body;
+const sendValidationError = (res, message) => res.status(StatusCodes.BAD_REQUEST)
+  .send(message);
 
-  console.log(userId, password);
+const loginValidateMiddleware = (req, res, next) => {
+  const { userId, password } = req.body || {};
 
   if (commonUtil.isEmpty(userId)) {
-    res.status(StatusCodes.UNAUTHORIZED)
-       .send(ErrorMessage.ID_NOT_NULL);
+    sendValidationError(res, ErrorMessage.ID_NOT_NULL);
     return;
   }
 
   if (commonUtil.isEmpty(password)) {
-    res.status(StatusCodes.UNAUTHORIZED)
-       .send(ErrorMessage.PASSWORD_NOT_NULL);
+    sendValidationError(res, ErrorMessage.PASSWORD_NOT_NULL);
     return;
   }
 
