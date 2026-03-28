af489b1d181430c0ad37eed27477c1a697a2332e
hahava <hahava5696@gmail.com>
2026-03-28 22:33:10 +0900

    refactor: modernize frontend entry and webpack config

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element (#root) not found');
+}
+
+ReactDOM.render(<App />, rootElement);
--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,34 +1,53 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  output: {
-    path: path.join(__dirname, '..') + '/back-end/server/front/',
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react'],
+module.exports = (_env, argv = {}) => {
+  const mode = argv.mode || 'development';
+  const isProduction = mode === 'production';
+
+  return {
+    mode,
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+      path: path.resolve(__dirname, '../back-end/server/front'),
+      filename: 'main.js',
+      clean: true,
+      publicPath: '/',
+    },
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    resolve: {
+      extensions: ['.js', '.json'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+      }),
     ],
-  },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'public'),
+      },
+      historyApiFallback: true,
+      hot: true,
+      port: 3000,
+    },
+  };
 };
